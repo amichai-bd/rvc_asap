951b107496d4ca3bb5acffeb0ccb7b670b18356a
amichaibd <amichaibd@gmail.com>
2022-05-24 22:19:36 +0300

    dont gitignore the list.f

---
+++ modelsim_run/rvc_asap_5pl_list.f
@@ -0,0 +1,13 @@
+//include dir - the Machine code for I_MEM load.
++incdir+../verif/
++incdir+../source/common
+
+//Source File (PKG, RTL, MACROS)
+../source/common/rvc_asap_pkg.sv
+../source/rvc_asap_5pl/rvc_asap_5pl.sv
+../source/rvc_asap_5pl/rvc_mem_wrap_5pl.sv
+../source/rvc_asap_5pl/rvc_top_5pl.sv
+
+//Test Bench
+../verif/rvc_asap_5pl_tb.sv
+
---
+++ modelsim_run/rvc_asap_sc_list.f
@@ -0,0 +1,13 @@
+//include dir - the Machine code for I_MEM load.
++incdir+../verif/
++incdir+../source/common
+
+//Source File (PKG, RTL, MACROS)
+../source/common/rvc_asap_pkg.sv
+../source/rvc_asap_sc/rvc_asap_sc.sv
+../source/rvc_asap_sc/rvc_mem_wrap_sc.sv
+../source/rvc_asap_sc/rvc_top_sc.sv
+
+//Test Bench
+../verif/rvc_asap_sc_tb.sv
+
